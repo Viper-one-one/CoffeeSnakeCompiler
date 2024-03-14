235a78c454cb2ffb157c8f6dca15bb6df4ea9091
Thomas Dinh <thomas98dinh@gmail.com>
2024-03-13 18:44:40 -0700

    Some commit I have to make

--- CoffeeSnakeCompiler.py
+++ CoffeeSnakeCompiler.py
@@ -1,6 +1,10 @@
 from Tokenizer.AdditionToken import AdditionToken
+from Tokenizer.Token import Token
 
 def main():
+    token = AdditionToken()
+    instance = isinstance(token, Token)
+    print(instance)
     print("CoffeeSnake")
     
 # this conditional runs the program when exe context is command line or as a file, it will NOT run under import module
--- Tests/test_tokenizer.py
+++ Tests/test_tokenizer.py
@@ -1,7 +1,12 @@
 import pytest
 from Tokenizer.AdditionToken import AdditionToken
+from Tokenizer.Token import Token
 
-def test_equality():
+def tokenizer(text):
+    tokens = text.split()
+    return tokens
+
+def test_add_equality():
     token1 = AdditionToken()
     token2 = AdditionToken()
     assert token1 == token2
