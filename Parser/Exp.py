7938a78b76b90c542af6d43baa6c789c63e9301f
ratherbefishingrightnow <bryan.avila.467@my.csun.edu>
2024-04-11 14:53:14 -0700

    WIP LHS Grammar Rules

--- Parser/Exp.py
+++ Parser/Exp.py
@@ -4,6 +4,21 @@ from abc import ABC
 class Exp(ABC):
     pass
 
+class IntegerLiteralExp(Exp(value)):
+    def __init__(self, value):
+        self.value = value
+
+class TrueExp(Exp):
+    pass
+
+class FalseExp(Exp):
+    pass
+
+class BinOpExp(leftExp, op, rightExp):
+    def __init__(self, leftExp, op, rightExp):
+        self.leftExp = leftExp
+        self.op = op
+        self.rightExp = rightExp
 
 
 
--- Parser/PrimaryExp.py
+++ Parser/PrimaryExp.py
@@ -4,6 +4,33 @@ from abc import ABC
 class PrimaryExp(ABC):
     pass
 
+class VariableExp(Exp):
+    pass
+
+class StringExp(Exp):
+    pass
+
+class IntegerExp(Exp):
+    pass
+
+class ParanExp(Exp):
+    pass
+
+class ThisExp(Exp):
+    pass
+
+class TrueExp(Exp):
+    pass
+
+class FalseExp(Exp):
+    pass
+
+class PrintLnExp(Exp):
+    pass
+
+class NewExp(Exp):
+    pass
+
 
 
 
