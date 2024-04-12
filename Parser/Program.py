aa5e42077751cfd359830acf5436c5fd8aa958b6
Viper-one-one <taylor.j.nastally@gmail.com>
2024-04-12 15:17:25 -0700

    final class fix

--- Parser/Program.py
+++ Parser/Program.py
@@ -1,13 +1,12 @@
-from abc import ABC
 from Parser.ClassDef import ClassDef
 from Parser.Statement import Statement 
 
 
-class Program(ABC):
+class Program:
     classDef: ClassDef
     statements:  Statement 
 
-    def __init__(self, classDef: ClassDef, statements: List[Statement]):
+    def __init__(self, classDef: ClassDef, statements: Statement) -> None:
        self.classDef = classDef
        self.statements = statements
 
