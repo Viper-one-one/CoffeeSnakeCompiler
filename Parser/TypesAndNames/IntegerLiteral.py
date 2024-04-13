9d240956acc28c52b68bd28f54d9571d1d91552a
Viper-one-one <taylor.j.nastally@gmail.com>
2024-04-13 13:30:41 -0700

    txt changes and small class changes

--- Parser/TypesAndNames/IntegerLiteral.py
+++ Parser/TypesAndNames/IntegerLiteral.py
@@ -1,3 +1,7 @@
+# This file contains the IntegerLiteral class, which is used to represent integer literals in the AST.
+# Differentiated from the Integer class, which is used to represent the integer type.
+# i.e. IntegerLiteral(5): 5 and Integer(5): Int
+
 class IntegerLiteral:
     value : int
     
--- Parser/TypesAndNames/Type.py
+++ Parser/TypesAndNames/Type.py
@@ -4,17 +4,23 @@ class Type(ABC):
     raise NotImplementedError("Type is an abstract class, it should not be instantiated")
 
 class IntType(Type):
-    def __init__(self) -> None:
-        pass
+    value: int
+    
+    def __init__(self, value: int) -> None:
+        self.value = value
 
 class BooleanType(Type):
-    def __init__(self) -> None:
-        pass
+    value: bool
+
+    def __init__(self, value: bool) -> None:
+        self.value = value
 
 class VoidType(Type):
     def __init__(self) -> None:
         pass
 
 class classname(Type):
+    
+
     def __init__(self) -> None:
         pass
